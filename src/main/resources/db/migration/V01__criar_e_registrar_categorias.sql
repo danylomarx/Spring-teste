7632cf0983bb1846ea9e3fe3afad02229d9be94b
danylo <danylomarx99@gmail.com>
2019-09-26 15:28:18 -0300

    'Preparacao'

---
+++ src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE categoria (
+codigo bigserial  primary key,
+nome varchar(50) NOT NULL
+);
+
+INSERT INTO categoria (nome) values ('Lazer');
+INSERT INTO categoria (nome) values ('alimentação');
+INSERT INTO categoria (nome) values ('supermercado');
+INSERT INTO categoria (nome) values ('Farmácia');
+INSERT INTO categoria (nome) values ('Outros');
---
+++ src/main/resources/db/migration/V02__criar_tabela_pessoa.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE pessoa (
+codigo bigserial  primary key,
+nome varchar(50) NOT NULL,
+ativo boolean NOT NULL,
+logradouro varchar(50),
+numero varchar(50),
+complemento varchar(50),
+bairro varchar(50),
+cep varchar(50),
+cidade varchar(50),
+estado varchar(50));
+
+INSERT INTO pessoa (nome,ativo,logradouro,numero,complemento,bairro,cep,cidade,estado) values ('Danylo Marx','true','casa','50','rua','Qng','72.130.340','Taguatinga','Brasília');
+INSERT INTO pessoa (nome,ativo,logradouro,numero,complemento,bairro,cep,cidade,estado) values ('Juliana Saueia','true','casa','23','rua','Zona Leste','50.000.000','Zona Norte','São Paulo');
+INSERT INTO pessoa (nome,ativo,logradouro,numero,complemento,bairro,cep,cidade,estado) values ('Marcos Andre','true','casa','66','rua','Qne','72.000.000','Taguatinga','Brasília');
+INSERT INTO pessoa (nome,ativo,logradouro,numero,complemento,bairro,cep,cidade,estado) values ('Vitor Alves','true','casa','50','rua','Qng','72.130.340','Taguatinga','Brasília');
+INSERT INTO pessoa (nome,ativo,logradouro,numero,complemento,bairro,cep,cidade,estado) values ('Anderson Farias','true','casa','H','Condominio','H','79.000.000','Valparaiso','Goiás');
+INSERT INTO pessoa (nome,ativo,logradouro,numero,complemento,bairro,cep,cidade,estado) values ('Marcus Felipe','true','casa','804','rua','Areal','53.130.340','Taguatinga','Brasília');
+INSERT INTO pessoa (nome,ativo,logradouro,numero,complemento,bairro,cep,cidade,estado) values ('Marcus Vinicius','true','casa','804','rua','areal','53.130.340','Taguatinga','Brasília');
+INSERT INTO pessoa (nome,ativo,logradouro,numero,complemento,bairro,cep,cidade,estado) values ('Valdirene Alves','true','casa','50','rua','Qng','72.130.340','Taguatinga','Brasília');
+INSERT INTO pessoa (nome,ativo,logradouro,numero,complemento,bairro,cep,cidade,estado) values ('Vilma Alves','true','casa','50','rua','Qng','72.130.340','Taguatinga','Brasília');
+INSERT INTO pessoa (nome,ativo,logradouro,numero,complemento,bairro,cep,cidade,estado) values ('Dalyla Thauanne','true','casa','50','rua','Qng','72.130.340','Taguatinga','Brasília');
